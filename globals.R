341767860eac56cbb2b419f581723fb32b7ee613
Amit Arora <aa1603@georgetown.edu>
2020-09-06 01:03:45 +0000

    add preface

---
+++ globals.R
@@ -0,0 +1,16 @@
+# Global constants
+P1_NAME <- "Nidhi"
+P2_NAME <- "Amit"
+
+P1_DATA_URL <- glue("https://raw.githubusercontent.com/aarora79/biomettracker/master/data/{P1_NAME}.csv")
+P2_DATA_URL <- glue("https://raw.githubusercontent.com/aarora79/biomettracker/master/data/{P2_NAME}.csv")
+
+# desired and ideal weights, ideal weight as per NIH wensite
+# https://www.nhlbi.nih.gov/health/educational/healthdisp/pdf/tipsheets/Are-You-at-a-Healthy-Weight.pdf
+P1_TARGET_WEIGHT <- 128
+P1_IDEAL_WEIGHT <- 127
+
+P2_TARGET_WEIGHT <- 190
+P2_IDEAL_WEIGHT <- 158
+
+CHART_ELEMENT_TEXT_SIZE <- 20
